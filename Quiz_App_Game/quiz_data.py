45a8e088f69f85b84758ec93c062ebe1063c163f
AreejFatima-cloud <fareej775@gmail.com>
2024-07-11 09:59:41 -0700

    Create quiz_data.py

---
+++ Quiz_App_Game/quiz_data.py
@@ -0,0 +1,63 @@
+# Sample data for the quizzes
+quiz_data = [
+    {
+        "question": "What is the capital of France?",
+        "answers": ["Paris", "London", "Berlin", "Madrid"],
+        "correct_answer": "Paris"
+    },
+    {
+        "question":"What is the largest planet in our solar system?",
+        "answers":["Jupiter","Saturn", "Mercury", "Earth"],
+        "correct_answer":"Jupiter"
+    },
+    {
+        "question":"What is the symbol of Potassium?",
+        "answers":["Ag","Au", "Po", "K"],
+        "correct_answer":"K"
+    },
+    {
+        "question":"Which country is known as the 'Land of the Rising Sun'?",
+        "answers":["Japan","Singapore", "Korea", "Thailand"],
+        "correct_answer":"Japan"
+    },
+    {
+        "question": "Who wrote 'To Kill a Mockingbird'?",
+        "answers": ["Harper Lee", "Mark Twain", "Ernest Hemingway", "J.K. Rowling"],
+        "correct_answer": "Harper Lee"
+    },
+    {
+        "question": "What is 5 * 6?",
+        "answers": ["11", "30", "28", "26"],
+        "correct_answer": "30"
+    },
+    {
+        "question": "What is 15 / 3?",
+        "answers": ["3", "5", "6", "7"],
+        "correct_answer": "5"
+    },
+    {
+        "question": "What is 2 + 2?",
+        "answers": ["3", "4", "5", "6"],
+        "correct_answer": "4"
+    },
+    {
+        "question": "What is the square root of 81?",
+        "answers": ["7", "8", "9", "10"],
+        "correct_answer": "9"
+    },
+    {
+        "question": "What is the holy book of Islam?",
+        "answers": ["Bible", "Torah", "Quran", "Vedas"],
+        "correct_answer": "Quran"
+    },
+    {
+        "question": "Who is the last prophet in Islam?",
+        "answers": ["Moses", "Jesus", "Muhammad", "Abraham"],
+        "correct_answer": "Muhammad"
+    },
+    {
+        "question": "How many pillars of Islam are there?",
+        "answers": ["3", "4", "5", "6"],
+        "correct_answer": "5"
+    }
+]
